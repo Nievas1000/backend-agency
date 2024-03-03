df9680e263c36a8a85925a851cbaff8454d0b311
Lautaro Nievas <lautynievas09@gmail.com>
2024-03-03 20:37:53 -0300

    nodemon removed for production

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({
-  origin: 'https://nevvedesign.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
