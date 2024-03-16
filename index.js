98f6c4aac4ab27f6156e3762e7561b005ad25b30
Lautaro Nievas <lautynievas09@gmail.com>
2024-03-16 15:42:04 -0300

    aproach changed to free website instead of guide

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const fs = require('fs');
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -14,13 +14,13 @@ app.use(express.json());
 app.use("/", router);
 
 const contactEmail = nodemailer.createTransport({
-  host: "smtpout.secureserver.net",  
+  host: "smtpout.secureserver.net",
   secure: true,
   secureConnection: false,
   tls: {
-      ciphers:'SSLv3'
+    ciphers: "SSLv3",
   },
-  requireTLS:true,
+  requireTLS: true,
   port: 465,
   debug: true,
   auth: {
@@ -43,7 +43,7 @@ router.post("/getQuote", (req, res) => {
   const message = req.body.message;
   const services = req.body.services.join(", ");
   const mail = {
-    from: 'lautaro.nievas@nevvedesign.com',
+    from: "lautaro.nievas@nevvedesign.com",
     to: "lautynievas09@gmail.com",
     subject: "Get quote needed",
     html: `<p>Name: ${name}</p>
@@ -61,11 +61,10 @@ router.post("/getQuote", (req, res) => {
   });
 });
 
-
-const confirmationEmail = (email) =>{
+const confirmationEmail = (email) => {
   const mail = {
-    from: 'lautaro.nievas@nevvedesign.com',
-    to: 'lautynievas09@gmail.com',
+    from: "lautaro.nievas@nevvedesign.com",
+    to: "lautynievas09@gmail.com",
     subject: "New Email From Nevve Funnel",
     text: `New customer to our email list: ${email}`,
   };
@@ -76,50 +75,36 @@ const confirmationEmail = (email) =>{
       res.json({ code: 200, status: "Email Saved" });
     }
   });
-  
-}
+};
 
 router.post("/salesFunnel", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
 
-
   const mailClient = {
-    from: 'lautaro.nievas@nevvedesign.com',
+    from: "lautaro.nievas@nevvedesign.com",
     to: email,
-    subject: 'How To Attract And Retain Clients From Nevve',
+    subject: "Free Website From Nevve",
     text: `
       Hello ${name}!
 
-      Thank you for requesting our free guide on how to convert prospects into customers. We are excited to share with you this valuable resource that will help you take your business to the next level!
-    
-      To download your guide, simply click on the document attached to the email.
-    
-      We hope you find the information useful and that it helps you achieve your business goals. If you have any questions or need additional help, please do not hesitate to contact us.
-    
-      Thanks again and have a fantastic day!
-    
-      Best Regards,
+      Thank you for your interest in our free Website service. We're thrilled to have you on board and want to assure you that your request is being processed diligently.
+
+      Please stay tuned as within the next 48 hours, you will receive the URL for your website. This valuable resource will be instrumental in propelling your business to new heights!
+      
+      If you have any immediate questions or need further assistance, feel free to reach out to us. We're here to support you every step of the way.
+      
+      Thank you once again for choosing us, and we look forward to assisting you in achieving your business goals.
+      
+      Best regards,
       Lautaro Nievas
       Nevve
     `,
-    attachments: [
-        {
-          filename: 'nevveGuideSpanish.pdf',
-          path: path.join(__dirname, './public/assets/NevveSpanish.pdf'),
-          contentType: 'application/pdf'
-        },
-        {
-          filename: 'nevveGuideEnglish.pdf',
-          path: path.join(__dirname, './public/assets/NevveEnglish.pdf'),
-          contentType: 'application/pdf'
-      }
-    ]
-  }
-  confirmationEmail(email)
+  };
+  confirmationEmail(email);
   contactEmail.sendMail(mailClient, (error) => {
     if (error) {
-      res.send({error});
+      res.send({ error });
     } else {
       res.send({ code: 200, status: "Message Sent" });
     }
